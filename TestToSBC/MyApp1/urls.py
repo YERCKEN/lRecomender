9f9c08b93eb7086481928ad47c2c1d83fdf8c2d8
EDWAR GONZALEZ <yercken30@gmail.com>
2024-05-23 10:21:54 -0500

    ❌ vista_recomemended.py ➕ Se cambió el nombre de la función de recomendación y se movió de 'vista_recomemended.py' a 'views' ➕ Se agregó en 'MyApp1/urls' la ruta como 'recomendar'

--- TestToSBC/MyApp1/urls.py
+++ TestToSBC/MyApp1/urls.py
@@ -2,14 +2,15 @@ from django.urls import path
 from . import views
 
 #IMPORAMOS VIEWS DE la carpeta vistas
-from .vistas import vista_recommended
 
 
 #URLS APP MyApp1
 
 urlpatterns = [
+    #INICIO
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
-    path('recomendar/', vista_recommended.recomendedVista, name='recomendar'),
+    #RECOMENDACIÓN
+    path('recomendar/', views.recomendar, name='recomendar'),
    
 ]
--- TestToSBC/MyApp1/views.py
+++ TestToSBC/MyApp1/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+# DEPENDENCIAS RECOMENDACIÓN
+
+from django.shortcuts import render
+from .recomenderFolder.recomender import get_recommendations
+from django.views.decorators.csrf import csrf_protect
 
 #HOME  =  = == = = == = = = = = = = = = == == = = = == = = = =
 def home(request):
@@ -10,3 +15,28 @@ def about (request):
     personas = [{'name1': 'Dilan Sobenis'}, {'name2': 'Edwar Gonzalez'}, {'name3':'Jeisson Paredes'}]
     return render (request, 'mi-plantilla-1.html', {'personas': personas})
 
+
+#RECOMENDACIÓN =  = == = = == = = = = = = = = = == == = = = == = = = =  = = = == = = = = = == = = = = = == = = =
+
+@csrf_protect
+def recomendar(request):
+    recommendations = None
+
+    if request.method == 'POST':
+        brand = request.POST.get('brand')
+        processor = request.POST.get('processor_brand')
+        ram = request.POST.get('ram_memory')
+        storage = request.POST.get('primary_storage_capacity')
+
+        
+        
+        filters = {
+            'brand': brand,
+            'processor_brand': processor,
+            'ram_memory': ram,
+            'primary_storage_capacity': storage,
+        }
+
+        recommendations = get_recommendations(filters)
+
+    return render (request, 'formulario.html', {'recommendations' : recommendations})
